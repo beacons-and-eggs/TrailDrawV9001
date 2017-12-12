1370a644a8408e0527d96366db12458d14decc95
Chris <choyek@ufl.edu>
2017-12-12 04:21:56 -0500

    This version of draw uses trail renderer instead of line renderer, and should work perfectly. It instantiates a trail when the user draws a new line (to fix the bug that would connect lines that were meant to be separate). The auto delete time can be set in destroy.cs (triggered onmouseup(0), when user lets go from button). Right now it's set to 5s.

---
+++ traildraw/Assets/destroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour {
+
+
+	private TrailRenderer tr;
+
+	void Start()
+	{
+		tr = GetComponent<TrailRenderer>();
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonUp (0)) {
+			tr.time = 5.0f; //how many seconds it takes to start deleting the trail
+		}
+			
+	}
+
+}
